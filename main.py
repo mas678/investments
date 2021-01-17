e8165898312a504d91af1b89539948af005cbb64
mas678 <57104317+mas678@users.noreply.github.com>
2021-01-17 23:14:57 +0300

    fixed bugs

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ a = [0] * n
 for i in range(1,n + 1):
     a[i - 1] = float(reader[i][3])
 k = int(input())
-b = [[0] * n for i in range(k)]
-dp = [[0] * n for i in range(k)]
-b_max = [0] * k
-a_max = 0
+b = [[float("-inf")] * n for i in range(k)]
+dp = [[float("-inf")] * n for i in range(k)]
+b_max = [float("-inf")] * k
+a_max = float("-inf")
 for i in range(n):
     
     for m in range(k):
