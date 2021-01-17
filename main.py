542a291feca84bee7b1f5891591e57c7555fa6c2
mas678 <57104317+mas678@users.noreply.github.com>
2021-01-17 22:47:21 +0300

    Investments first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import csv
+ 
+File = open('new.csv', newline='')
+reader = list(csv.reader(File))
+n = len(reader) - 1
+a = [0] * n
+for i in range(1,n + 1):
+    a[i - 1] = float(reader[i][3])
+k = int(input())
+b = [[0] * n for i in range(k)]
+dp = [[0] * n for i in range(k)]
+b_max = [0] * k
+a_max = 0
+for i in range(n):
+    
+    for m in range(k):
+        if m != 0:
+            dp[m][i] = max(a[i] + b_max[m - 1], dp[m][i - 1])
+        else:
+            dp[m][i] = max(a[i] + a_max, dp[m][i - 1])
+        b[m][i] = dp[m][i] - a[i]
+        b_max[m] = max(b_max[m], b[m][i])
+        a_max = max(a_max, -a[i])
+print(dp[-1][-1])
